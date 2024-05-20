b4ff7f485a7849ab47228980108bef23b6b4c5a1
acb94981 <acb94981@gmail.com>
2024-05-20 10:36:43 +0330

    edit2

--- DownloadManager.sh
+++ DownloadManager.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
+#DownloadManager tool
 
+read -p "please give me Url : " answer
 
+answer2=answer
+
+if [ "$answer2" = Url ];then
+    echo "OK! i try it!"
+    echo -ne '#####                     (33%)\r'
+    sleep 1
+    echo -ne '#############             (66%)\r'
+    sleep 1
+    echo -ne '#######################   (100%)\r'
+    echo -ne '\n'
+    curl -O answer2
+
+fi
